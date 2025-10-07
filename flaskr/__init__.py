de89f843c81b7cdeb9540a03e733564303c5b435
FaseehFS <faseeh1080@gmail.com>
2025-10-07 09:28:40 +0530

    Remove add bp

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,7 @@
 import os
 
-from flask import Flask, url_for
+from flask import Flask, Blueprint, render_template, request, url_for
+from .db import get_db
 
 
 def create_app(test_config=None):
@@ -24,13 +25,51 @@ def create_app(test_config=None):
         pass
 
     from . import db
-    from . import add
 
     db.init_app(app)
-    app.register_blueprint(add.bp)
+
+    # ---
 
     @app.route("/")
     def home():
-        return f"<a href='{url_for('add.add')}'>Add</a>"
+        return f"<a href='{url_for('add')}'>Add</a>"
+
+    @app.route("/add/", methods=("GET", "POST"))
+    def add():
+        """
+        GET: Returns the add page.
+        POST: Accepts a FormData containing "word" and "comments" as the body.
+        """
+        if request.method == "GET":
+            return render_template("add.html")
+
+        word = request.form.get("word").strip().lower()
+        if word is None:
+            return "Word is empty.", 400
+
+        comments = request.form.get("comments", "").strip()
+
+        db = get_db()
+        cursor = db.cursor()
+        try:
+            cursor.execute(
+                "SELECT EXISTS(SELECT 1 FROM words WHERE word = %s)", (word,)
+            )
+            exists = cursor.fetchone()[0]
+            if exists:
+                return "Word already exists", 400
+
+            cursor.execute(
+                """
+                INSERT INTO words (word, comment, interval_days)
+                VALUES (%s, %s, %s)
+                """,
+                (word, comments, 1),  # interval_days starts at 1
+            )
+            db.commit()
+        except:
+            return "Failed to add the word.", 400
+
+        return "Word added."
 
     return app
