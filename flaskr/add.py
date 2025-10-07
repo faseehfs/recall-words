1da0595ccc4507239351a6afdd54bfb84b9777e3
FaseehFS <faseeh1080@gmail.com>
2025-10-07 08:17:19 +0530

    Make add page add words to the db

--- flaskr/add.py
+++ flaskr/add.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request
+from .db import get_db
 
 bp = Blueprint("add", __name__, url_prefix="/add")
 
@@ -8,5 +9,27 @@ def add():
     if request.method == "GET":
         return render_template("add.html")
 
-    print(request.data.decode())
+    json = request.get_json()
+    word = json.get("word").strip().lower()
+    comments = json.get("comments").strip()
+
+    db = get_db()
+    cursor = db.cursor()
+    try:
+        cursor.execute("SELECT EXISTS(SELECT 1 FROM words WHERE word = %s)", (word,))
+        exists = cursor.fetchone()[0]
+        if exists:
+            return "Word already exists", 400
+
+        cursor.execute(
+            """
+            INSERT INTO words (word, comment, interval_days)
+            VALUES (%s, %s, %s)
+            """,
+            (word, comments, 1),  # interval_days starts at 1
+        )
+        db.commit()
+    except:
+        return "Failed to add the word.", 400
+
     return "Success"
